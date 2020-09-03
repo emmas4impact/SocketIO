64aa884f3e50e006c56ca1830425ea0be1ae9bce
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-03 17:13:55 +0300

    Update rooms.js

---
+++ schemas/rooms.js
@@ -0,0 +1,13 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const RoomSchema = new Schema({
+    name: String,
+    members: [{
+        username: String,
+        id: String,
+    }],
+})
+const RoomModel = mongoose.model("room", RoomSchema)
+
+module.exports =RoomModel
